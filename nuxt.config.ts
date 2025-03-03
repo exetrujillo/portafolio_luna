e2ac0ba109dfc3366fd71606b9a86805bbda0166
exetrujillo <exequiel.trujillo@ug.uchile.cl>
2025-03-03 18:54:14 -0300

    Deploy app to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,28 @@ export default defineNuxtConfig({
 
   // Base URL para GitHub Pages
   app: {
-    baseURL: '/'
+    baseURL: '/portafolio_luna/',  // Esto es válido para la base URL en la sección app
   },
+
+  // Configuración de los módulos (ahora todos se agregan aquí)
+  modules: [
+    // Aquí incluir módulos
+  
+  ],
+
+  build: {
+    // Aquí puedes dejar configuraciones específicas de build como transpile
+    transpile: [],
+  },
+
+  vite: {
+    // Aquí se usa la propiedad `base` en lugar de `publicPath`
+    base: '/portafolio_luna/', // Ruta base para todos los recursos estáticos
+    build: {
+      assetsDir: '_nuxt', // Establece la carpeta de los activos generados
+    }
+  },
+
   nitro: {
     prerender: {
       routes: ['/'] // Renderiza la página de inicio
